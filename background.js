3f20b3d9873692f6c0b1395831e0731445540afa
Romario322 <132849490+Romario322@users.noreply.github.com>
2023-10-08 13:26:05 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 let body = document.getElementById("body")
-let deg = 30
+let deg = Math.floor(Math.random() * 360)
+let colors = ["rgb(216, 36, 12), 20%, rgb(63, 9, 77), 80%, rgb(5, 184, 145)",
+              "rgb(114, 9, 175), 30%, rgb(8, 160, 89), 70%, rgb(148, 139, 14)"]
+let color = colors[Math.floor(Math.random() * colors.length)]
 
 setInterval(function(){
-    body.style.background = `linear-gradient(${deg}deg, rgb(216, 36, 12), 20%, rgb(63, 9, 77), 80%, rgb(5, 184, 145)) fixed`
+    body.style.background = `linear-gradient(${deg}deg, ${color}) fixed`
     deg += 0.05
 }, 10)
