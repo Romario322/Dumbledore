731c64728774b7e1debf2d489555a4bad7c58a84
Romario322 <roma555silkin@gmail.com>
2024-07-29 21:05:44 +0300

    change

---
+++ js_scripts/theme_change.js
@@ -0,0 +1,21 @@
+try{
+    let theme_changer = document.getElementById("Theme_changer");
+    let Theme_link = document.getElementById("Theme_link");
+}
+catch{}
+theme_changer.onclick = function(){
+    let theme = Theme_link.getAttribute("href");
+    if (theme == "../Styles/light_theme.css") {
+        theme_changer.textContent = "Dark";
+        document.cookie = `Theme= ${"../Styles/dark_theme.css"}; path=/; max-age=${3600*24*30}`;
+        Theme_link.removeAttribute('href');
+        Theme_link.setAttribute("href", "../Styles/dark_theme.css");
+    }
+    else{
+        theme_changer.textContent = "Light";
+        document.cookie = `Theme= ${"../Styles/light_theme.css"}; path=/; max-age=${3600*24*30}`;
+        Theme_link.removeAttribute('href');
+        Theme_link.setAttribute("href", "../Styles/light_theme.css");
+    }
+    console.log(theme);
+}
